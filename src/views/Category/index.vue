85d792ca58be2bbdfa5606bbc94f8f430ca83da2
Zhuhaoran111 <1986517916@qq.com>
2023-05-18 23:01:16 +0800

    业务逻辑封装

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,45 +1,17 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { getCategoryApi } from '@/api/category'
-import { getBannerApi } from '@/api/home'
-import { onBeforeRouteUpdate } from 'vue-router'
 
-//引入路由
-import { useRoute } from 'vue-router'
+//引入goodsItems组件
+import goodsItem from '@/views/Home/components/goodsItem.vue'
 
-const cateGoryData = ref({})
-const route = useRoute()  //获取实例
-//这里的id默认要是route.params.id，传了就用最新的数据
-const getCateGoryList = async (id = route.params.id) => {
-    //route.params.id-----parmas就用params,query就用query
-    const res = await getCategoryApi(id)
-    cateGoryData.value = res.result
+//引入轮播图封装的函数
+import { useBannner } from './composables/useBannner'
+//解构bannerList的值
+const { bannerList } = useBannner()
 
-}
-const bannerList = ref([])
-//获取轮播图接口数据
-const getBanner = async () => {
-    const res = await getBannerApi(
-        {
-            distributionSite: '2'
-        }
-    )
-    bannerList.value = res.result
-}
-
-onMounted(() => {
-    getCateGoryList() //获取分类列表·
-    getBanner() //获取轮播图
-})
-
-//目标：路由参数变化的时候，可以把分类数据接口重新发送
-//to是目标路由对象，里面有params最新的参数
-onBeforeRouteUpdate((to) => {
-    //存在问题：要使用最新的路由参数请求最新的分类数据
-    getCateGoryList(to.params.id)
-    //把最新的路由id带过去
-})
-
+//引入分类封装的函数
+import { useCategory } from './composables/useCategory'
+//解构cateGoryData的值
+const { cateGoryData } = useCategory()
 
 </script>
 
@@ -83,10 +55,6 @@ onBeforeRouteUpdate((to) => {
                     <goodsItem v-for="goods in item.goods" :goods="goods" :key="goods.id"></goodsItem>
                 </div>
             </div>
-
-
-
-
         </div>
     </div>
 </template>
